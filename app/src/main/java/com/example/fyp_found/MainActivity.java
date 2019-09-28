08a5dad350aac437ed0434ae0755efa52a078d4e
chauchauhy <50822564+chauchauhy@users.noreply.github.com>
2019-09-28 22:38:45 +0800

    connect to facebook

--- app/src/main/java/com/example/fyp_found/MainActivity.java
+++ app/src/main/java/com/example/fyp_found/MainActivity.java
@@ -12,6 +12,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // this is setup activity for inittial SDK(facebook and firebase and google sdk
+        // please dont coding here
+
 
     }
 
